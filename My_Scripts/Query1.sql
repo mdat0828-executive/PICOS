2597d76a574dcf70550e7b7c5503dd14cc9cc946
mrdat0828-executive <mr.dat0828@gmail.com>
2026-06-09 09:57:37 +0700

    Update

--- My_Scripts/Query1.sql
+++ My_Scripts/Query1.sql
@@ -1,24 +1,26 @@
-with
+WITH
 	GT_Audit as
-				(SELECT 
-						Month,ZoneID,SRID,SRFullName
-						,OutletID,AuditDate,Name,SegmentName,Tier
-						,NND_Target,NND_Actual,NND
-						,FS_Target,FS_Actual,FS
-						--,FS_Target2,FS_Actual2,FS2
-						,VS_Target,VS_Actual,VS
-						,PrCom_Target,PrCom_Actual,PrCom
-						,ProAc_Target,ProAc_Actual,ProAc
-				 FROM [srp].[SRP-EOEAnswerFocusPerformanceAudit_SRP] ),
+		(SELECT 
+			Month,ZoneID,SRID,SRFullName
+			,OutletID,Name,AuditDate,SegmentName,Tier
+			,NND_Target,NND_Actual,NND
+			,FS_Target,FS_Actual,FS
+			,FS_Target2 as [FS_OS_Target],FS_Actual2 as [FS_OS_Actual] ,FS2 as [FS_OS]
+			,VS_Target,VS_Actual,VS
+			,PrCom_Target,PrCom_Actual,PrCom
+			,ProAc_Target,ProAc_Actual,ProAc
+		FROM [srp].[SRP-EOEAnswerFocusPerformanceAudit_SRP] ),
 	MT_Audit as
-				(SELECT
-					Month,ZoneID,SRID,SRFullName
-					,AuditDate,OutletID,Name,SegmentName,Tier
-					,NND_Target,NND_Actual,NND
-					,FS_Target,FS_Actual,FS
-					,FS_Target2,FS_Actual2,FS2
-					,VS_Target,VS_Actual,VS
-					,PrCom_Target,PrCom_Actual,PrCom
-				FROM [srp].[SRP-EOEAnswerFocusPerformanceAudit])
+		(SELECT
+			Month,ZoneID,SRID,SRFullName
+			,OutletID,Name,AuditDate,SegmentName,Tier
+			,NND_Target,NND_Actual,NND
+			,FS_Target,FS_Actual,FS
+			,FS_Target2 as [FS_OS_Target],FS_Actual2 as [FS_OS_Actual] ,FS2 as [FS_OS]
+			,VS_Target,VS_Actual,VS
+			,PrCom_Target,PrCom_Actual,PrCom
+			,NULL ProAc_Target, NULL  ProAc_Actual, NULL ProAc
+		FROM [srp].[SRP-EOEAnswerFocusPerformanceAudit])
 SELECT TOP 5 * FROM GT_AUDIT WHERE MONTH = '2026-05-01'
---SELECT TOP 5 * FROM MT_AUDIT WHERE MONTH = '2026-05-01'
+UNION ALL
+SELECT TOP 5 * FROM MT_AUDIT WHERE MONTH = '2026-05-01'
