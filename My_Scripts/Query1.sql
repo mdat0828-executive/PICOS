20e8ff790007ee8f72c56dd1b042e92502ddef57
mrdat0828-executive <mr.dat0828@gmail.com>
2026-06-09 17:19:13 +0700

    Update 06062026

--- My_Scripts/Query1.sql
+++ My_Scripts/Query1.sql
@@ -1,8 +1,8 @@
 WITH
 /*=========================Bảng lấy Data Audit Kênh GT - BU: NORTH|CENTRAL|GHCM|MKD (TOFT/TONT/MONT)=========================*/
 	GT_Audit as
-		(SELECT 
-			FORMAT(Month,'yyyy-MM') as [Month],ZoneID,SRID,SRFullName
+		(SELECT 'GT' as Channel
+			,FORMAT(Month,'yyyy-MM') as [Month],ZoneID,SRID,SRFullName
 			,OutletID,TRIM(Name) as [OutletName],FORMAT(AuditDate,'dd/MM/yyyy') as AuditDate,SegmentName,Tier
 			,NND_Target,NND_Actual,NND
 			,FS_Target,FS_Actual,FS
@@ -15,8 +15,8 @@ WITH
 
 /*=========================Bảng lấy Data Audit Kênh MONT - BU: MONT=========================*/
 	MONT_Audit as
-		(SELECT 
-			FORMAT(Month,'yyyy-MM') as [Month],ZoneID,SRID,SRFullName
+		(SELECT 'MONT' as Channel
+			,FORMAT(Month,'yyyy-MM') as [Month],ZoneID,SRID,SRFullName
 			,OutletID,TRIM(Name) as [OutletName],FORMAT(AuditDate,'dd/MM/yyyy') as AuditDate,SegmentName,Tier
 			,NND_Target,NND_Actual,NND
 			,FS_Target,FS_Actual,FS
@@ -29,8 +29,8 @@ WITH
 
 /*================================Bảng lấy Data Audit Kênh MT - BU: MOFT=================================*/
 	MT_Audit as
-		(SELECT
-			FORMAT(Month,'yyyy-MM') as [Month],ZoneID,SRID,SRFullName
+		(SELECT 'MOFT' as Channel
+			,FORMAT(Month,'yyyy-MM') as [Month],ZoneID,SRID,SRFullName
 			,OutletID,TRIM(Name) as [OutletName],FORMAT(AuditDate,'dd/MM/yyyy') as AuditDate,SegmentName,Tier
 			,NND_Target,NND_Actual,NND
 			,FS_Target,FS_Actual,FS
@@ -38,7 +38,8 @@ WITH
 			,VS_Target,VS_Actual,VS
 			,PrCom_Target,PrCom_Actual,PrCom
 			,NULL ProAc_Target, NULL  ProAc_Actual, NULL ProAc
-		FROM [srp].[SRP-EOEAnswerFocusPerformanceAudit]),
+		FROM [srp].[SRP-EOEAnswerFocusPerformanceAudit]
+		WHERE MONTH >'2024-12-01'),
 
 /*================================Bảng lấy Data Audit NTW=================================*/
 	Total_Audit as
@@ -50,24 +51,51 @@ WITH
 
 /*==============================Bảng Target PICOS theo Tháng===============================*/
 	GT_Target as
-		(SELECT 
-			Channel
-			,FORMAT(Month,'yyyy-MM') as Month
+		(SELECT
+			FORMAT(Month,'yyyy-MM') as Month
+			,Channel
 			,Audited_Outlet [OutletTarget]
 			,Perfect_Outlet [OutletPerfect]
 			,Target_PICOS [%Perfect]
 		FROM [srp].[TF_PICOSTarget_GT&MONT]),
 --============================================
 	MT_Target as
-		(Select
-			FORMAT(Month,'yyyy-MM') as Month
+		(Select 'MOFT' as Channel
+			,FORMAT(Month,'yyyy-MM') as Month
 			,ZoneID
 			,Audited_Outlet [OutletTarget]
 			,Perfect_Outlet [OutletPerfect]
 			--,SRP_Outlet
 			,Target_PICOS [%Perfect]
 			,Target_Fundamental [%Fundamental]
-		FROM [srp].[TF_PICOSTarget_MOFT])
+		FROM [srp].[TF_PICOSTarget_MOFT]),
 --============================================
 	MONT_Target as
-SELECT TOP 10 * FROM MT_Target WHERE Month = '2026-04'
+		(SELECT
+			FORMAT(Month, 'yyyy-MM') AS [Month]
+			,Channel
+			,Audited_Outlet AS OutletTarget
+			,Perfect_Outlet AS OutletPerfect
+			,Target_PICOS AS [%Perfect]
+		FROM [srp].[TF_PICOSTarget_GT&MONT]
+		WHERE Month < '2026-06-01'
+			UNION ALL
+	/*==============Update Target Mới nếu có thay đổi==============*/
+		SELECT DISTINCT
+			FORMAT(Month, 'yyyy-MM') AS [Month]
+			,'MONT' AS Channel
+			,40 AS OutletTarget
+			,30 AS OutletPerfect
+			,0.75 AS [%Perfect]
+		FROM [srp].[TF_PICOSTarget_GT&MONT]
+		WHERE Month = '2026-06-01')
+
+/*================================Bảng lấy TARGET Audit NTW=================================*/
+--SELECT * FROM GT_Target WHERE Month = '2026-06'
+--UNION ALL
+--SELECT * FROM MT_Target WHERE Month = '2026-06'
+--UNION ALL
+--SELECT * FROM MONT_Target WHERE Month = '2026-06'
+
+
+SELECT * FROM MT_Audit WHERE Month = '2026-05'
