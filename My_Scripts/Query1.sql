dae2999040a5d098bcb79f8c5e3316ba2cee85c3
mrdat0828-executive <mr.dat0828@gmail.com>
2026-06-10 18:05:46 +0700

    Update Query1.sql

--- My_Scripts/Query1.sql
+++ My_Scripts/Query1.sql
@@ -107,25 +107,80 @@ Set @Month = '2026-05-01'
 		FROM [srp].[TF_PICOSTarget_GT&MONT]
 		WHERE Month = '2026-06-01'),
 /*----------------------------------------------------------------------------| Query chạy rule tính |--------------------------------------------------------------------------------*/
-	Result_PICOS_GT as
+	PICOS_GT as
 		(SELECT 
-			g.*
-			,tg.[OutletTarget]
-			,tg.[OutletPerfect]
-			,tg.[%Perfect]
-			FROM GT_AUDIT as g
-			left join GT_Target as tg on tg.Month = g.[Month]
-		--WHERE g.Month = @Month
-		UNION ALL
-		SELECT 
-			MO.*
-			,tg1.[OutletTarget]
-			,tg1.[OutletPerfect]
-			,tg1.[%Perfect]
-			FROM MONT_AUDIT as MO
-			left join MONT_Target as tg1 on tg1.Month = MO.[Month]
-		--WHERE MO.Month = @Month
-		)
+			t.*
+			,CASE WHEN Note_SRP IS NULL THEN -1 ELSE Note_SRP END as Note_SRP
+			FROM
+				(SELECT *
+					FROM GT_AUDIT
+				UNION ALL
+				SELECT *
+					FROM MONT_AUDIT
+				)t
+				LEFT JOIN Structured as s on t.Month = s.Month and t.SRID = s.PersonID
+		),
 
-	
-		SELECT * FROM Result_PICOS_GT WHERE MONTH = @MONTH and ZoneID like ('MO_')
+	Result_Picos_GT as
+		(SELECT *
+				,CASE 
+					WHEN [Month] IN ('2026-04-01','2026-05-01')
+						 AND isnull(NND_Result,1) <> 0
+						 AND isnull(VS_Result,1) <> 0
+						 AND isnull(FS_Result,1) <> 0
+						 AND isnull(PrCom_Result,1) <> 0
+					THEN 1
+					WHEN [Month] NOT IN ('2026-04-01','2026-05-01')
+						 AND isnull(NND_Result,1) <> 0
+						 AND isnull(VS_Result,1) <> 0
+						 AND isnull(FS_Result,1) <> 0
+						 AND isnull(PrCom_Result,1) <> 0
+						 AND isnull(ProAc_Result,1) <> 0
+					THEN 1
+					ELSE 0
+				END AS PerfectStore
+		FROM
+			(SELECT
+				Month ,ZoneID ,SRID ,SRFullName ,AuditDate
+				,OutletID ,OutletName  ,SegmentName ,Tier ,Note_SRP
+				,CASE
+					WHEN NND_Target IS NULL THEN NULL
+						WHEN NND_Actual >= NND_Target THEN 1
+							ELSE 0
+								END AS NND_Result
+				,CASE
+					WHEN FS_Target IS NULL THEN NULL
+						WHEN FS_Actual >= FS_Target THEN 1
+							ELSE 0
+								END AS FS_Result
+				,CASE
+					WHEN VS_Target IS NULL THEN NULL
+						WHEN VS_Actual >= VS_Target THEN 1
+							ELSE 0
+								END AS VS_Result
+				,CASE
+					WHEN PrCom_Target IS NULL THEN NULL
+						WHEN PrCom_Actual >= PrCom_Target THEN 1
+							ELSE 0
+								END AS PrCom_Result
+				,CASE
+					WHEN ProAc_Target IS NULL THEN NULL
+						WHEN ProAc_Actual >= ProAc_Target THEN 1
+							ELSE 0
+								END AS ProAc_Result
+				FROM PICOS_GT) t)
+		
+/*=========>>>> */ 
+SELECT
+ZONEID
+,SRID
+,SRFULLNAME
+,SUM(NND_RESULT)
+,SUM(FS_RESULT)
+,SUM(VS_RESULT)
+,SUM(PRCOM_RESULT)
+,SUM(PROAC_RESULT)
+FROM Result_Picos_GT WHERE MONTH = @MONTH
+GROUP BY ZONEID
+,SRID
+,SRFULLNAME
